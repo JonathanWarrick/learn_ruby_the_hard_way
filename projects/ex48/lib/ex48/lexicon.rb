6ac9894aec34a1c0349abb19c2ea764430d4ae17
Jonathan Warrick <jonathan.warrick@gmail.com>
2014-12-24 10:22:27 -0600

    Completes Exercise 48.

--- projects/ex48/lib/ex48/lexicon.rb
+++ projects/ex48/lib/ex48/lexicon.rb
@@ -1,22 +1,45 @@
 class Lexicon
-	def scan(name)
-		return name
+	def initialize
+		@word_types = ['direction', 'verb', 'stop', 'noun']
+		@lexicon = {
+			'direction' => ['north', 'south', 'east', 'west', 'up', 'down', 'left', 'right', 'back'],
+			'verb' => ['go', 'stop', 'eat', 'kill'],
+			'stop' => ['the', 'in', 'of', 'from', 'at', 'it'],
+			'noun' => ['door', 'bear', 'princess', 'cabinet'],
+		}
+	end
+
+	def convert_number(word) 
+		begin
+			return Integer(word)
+		rescue
+			return nil
+		end
+	end
+
+	def scan(words)
+		converted_words = words.split(' ')
+		pairs = []
+		converted_words.each do |word| 
+			pairs.push(get_word_type(word))
+		end
+		return pairs
 	end
 
 	def get_word_type(word)
-		word_types.each do |type|
-			lexicon[type].index(word)? return type
-		return nil
-
-	word_types = ['direction', 'verb', 'stop', 'noun', 'number']
-
-	lexicon = {
-		'direction' => ['north', 'south', 'east', 'west', 'up', 'down', 'left', 'right', 'back'],
-		'verb' => ['go', 'stop', 'eat', 'kill'],
-		'stop' => ['the', 'in', 'of', 'from', 'at', 'it'],
-		'noun' => ['door', 'bear', 'princess', 'cabinet']
-	# 	# 'number' => /[0-9]/
-	}
+		word_type = 'error'
+		if convert_number(word) != nil
+			word_type = 'number'
+			word = convert_number(word)
+		else
+			@word_types.each do |type|
+				if @lexicon[type].index(word) != nil
+					word_type = type
+				end
+			end
+		end
+		return [word_type, word]
+	end
 
 # stuff = $stdin.gets.chomp
 # words = stuff.split
